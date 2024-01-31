3797a6d67f137341dea00e8d2cb946f63936c6df
Maycon-alt <54949734+Maycon-alt@users.noreply.github.com>
2024-01-31 12:07:04 -0300

    api.js

--- api.js
+++ api.js
@@ -16,39 +16,6 @@ const retornaProdutos = async ()=>{
     } catch (error) {
         console.error('Ocorreu um erro:', error);
     }
-    // try {
-    //     const resposta = await fetch('https://fakestoreapi.com/products/${id}');
-    //     const data = await resposta.json();
-
-    //     data.forEach(item => {
-    //         criarCard(item.conteudo);
-    //     });
-    // } catch (error) {
-    //     console.error('Ocorreu um erro:', error);
-    // }
-    
-    
-    // const imagem = document.getElementById('imagem');
-
-    // const titulo = document.getElementById('titulo');
-
-    // const preco = document.getElementById('preco');
-
-    // const categoria = document.getElementById('categoria');
-
-    // const nota = document.getElementById('nota');
-
-    // const {image,description,title,price,category,rating} = dado;
-
-    // imagem.innerHTML = `<img src='${image}' alt='${description}'/>`
-
-    // titulo.innerText = title;
-
-    // preco.innerText = 'preco: '+price;
-
-    // categoria.innerText = 'categoria: '+category;
-
-    // nota.innerText = 'nota: '+rating.rate;
 }
 function criarCard(conteudo) {
     console.log(conteudo.image)
